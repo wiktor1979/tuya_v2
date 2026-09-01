186140aa6da99134ca7308382c07a357301afc90
wiktor1979 <wiktorkmieciak@gmail.com>
2026-09-01 13:59:22 +0200

    jakieś zmiany

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-
-# 1. Uruchomienie zbierania danych w tle
-python main.py &
-
-# 2. Glowny proces (Streamlit) na pierwszym planie
-exec python -m streamlit run Panel.py --server.port 8501 --server.address 0.0.0.0
